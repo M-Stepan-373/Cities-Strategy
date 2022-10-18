7aedb6adc8ed1453f91a4cb4e4dc0804160d6994
M-Stepan-373 <step3212m@gmail.com>
2022-10-18 08:39:38 +0300

    add Rules

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 cities = {
-    'London': [3, 0],
-    'Edinburg': [2, 0],
-    'Dublin': [1, 0],
+    'Moskva': [1, 0],
 }
 
 review = 'all'
@@ -13,21 +11,43 @@ resources = {
 
 while True:
     if review == 'all':
+        print('Total adult people -', sum(
+            val[0]
+            for val in cities.values()
+        ))
         for name, amount in resources.items():
             print(name, '=', amount)
         print()
-        print('0 - New City')
-        print('1 - End turn')
+        print('0 - End turn')
+        print('1 - New City')
+        print('2 - Rules')
+
+
         for i, name in enumerate(cities):
-            print(i + 2, '-', name)
+            print(i + 3, '-', name)
         choice = int(input())
 
-        if choice == 0:
+        if choice == 2:
+            print('''
+Есть города, а также два ресурса - еда и дерево.
+У городов есть население взрослое, и молодое,
+заплатив 5 еды можно увеличить молодое
+население на 1, при этом молодого населения
+не может быть больше взрослого населения : 2
+с округлением в большую сторуну. Также можно
+создать новый город, это требует 10 дерева,
+в нём будет сразу 1 взрослое население. В конце
+хода за каждое взрослое население будет на 1
+возростать дерево и еда, также всё молодое
+население станет взрослым.''', end='')
+            input()
+
+        if choice == 1:
             if resources['wood'] > 9:
                 resources['wood'] -= 10
                 cities[input('Name City:\n')] = [1, 0]
 
-        if choice == 1:
+        if choice == 0:
             resources['food'] += sum(
                 val[0]
                 for val in cities.values()
@@ -42,7 +62,7 @@ while True:
                 i[0] += i[1]
                 i[1] = 0
 
-        number = 2
+        number = 3
         for i in cities:
             if number == choice:
                 review = 'city_' + i
